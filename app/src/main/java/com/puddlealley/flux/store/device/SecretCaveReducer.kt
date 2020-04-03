b93d280b39fe938035f5f921df10af10ece56f4f
Jonathon Smyth <jsmyth886@gmail.com>
2020-04-03 21:53:22 +0100

    Update SecretCaveReducer and add tests
    Update SecretCaveReducer to return the correct values when the actions are of the correct type (otherwise return the currently stored value)
    Add UnitTests for the Reducer which covers each of the reduce methods and and each of the states they can return.

--- app/src/main/java/com/puddlealley/flux/store/device/SecretCaveReducer.kt
+++ app/src/main/java/com/puddlealley/flux/store/device/SecretCaveReducer.kt
@@ -3,7 +3,6 @@ package com.puddlealley.flux.store.device
 import com.puddlealley.flux.service.CodeVerificationResult
 import com.puddlealley.splash.core.Action
 import com.puddlealley.splash.core.createReducer
-import timber.log.Timber
 
 val secretCaveReducer = createReducer<SecretCaveState> { action ->
     copy(
@@ -15,15 +14,23 @@ val secretCaveReducer = createReducer<SecretCaveState> { action ->
 
 fun reduceEnteredCode(enteredCode: String, action: Action): String =
     when (action) {
+        is SecretCaveEvents.LetteredEntered -> enteredCode + action.letter
+        is SecretCaveEvents.CodeEntered -> action.code
         else -> enteredCode
     }
 
 fun reducerLoading(loading: Boolean, action: Action): Boolean =
     when (action) {
+        CodeVerificationResult.Loading -> true
+        is CodeVerificationResult.Success -> false
+        is CodeVerificationResult.Error -> false
         else -> loading
     }
 
 fun reduceCodeCorrect(codeCorrect: Boolean, action: Action): Boolean =
     when (action) {
+        CodeVerificationResult.Loading -> false
+        is CodeVerificationResult.Success -> action.correct
+        is CodeVerificationResult.Error -> false
         else -> codeCorrect
     }
---
+++ app/src/test/java/com/puddlealley/flux/SecretCaveReducerTest.kt
@@ -0,0 +1,73 @@
+package com.puddlealley.flux
+
+import com.puddlealley.flux.service.CodeVerificationResult
+import com.puddlealley.flux.store.device.SecretCaveEvents
+import com.puddlealley.flux.store.device.reduceCodeCorrect
+import com.puddlealley.flux.store.device.reduceEnteredCode
+import com.puddlealley.flux.store.device.reducerLoading
+import com.puddlealley.splash.core.Event
+import junit.framework.Assert.*
+import org.junit.Test
+
+class SecretCaveReducerTest {
+
+    inner class UnknownEvent: Event
+
+    //reduceEnteredCode tests
+    @Test
+    fun `When action is LetteredEntered, reduceEnteredCode should return the currently entered code plus the new letter entered`() {
+        assertEquals("ABC", reduceEnteredCode("AB", SecretCaveEvents.LetteredEntered("C")))
+    }
+
+    @Test
+    fun `When action is CodeEntered, reduceEnteredCode should return the code`() {
+        assertEquals("ABC", reduceEnteredCode("AB", SecretCaveEvents.CodeEntered("ABC")))
+    }
+
+    @Test
+    fun `When action isn't of known type, reduceEnteredCode should return the currently entered code`() {
+        assertEquals("DEF", reduceEnteredCode("DEF", UnknownEvent()))
+    }
+
+    //reduceLoading tests
+    @Test
+    fun `When CodeVerificationResult is Loading, reduceLoading should return true`() {
+        assertTrue(reducerLoading(false, CodeVerificationResult.Loading))
+    }
+
+    @Test
+    fun `When CodeVerificationResult is Success, reduceLoading should return false`() {
+        assertFalse(reducerLoading(true, CodeVerificationResult.Success(true)))
+    }
+
+    @Test
+    fun `When CodeVerificationResult is Error, reduceLoading should return false`() {
+        assertFalse(reducerLoading(true, CodeVerificationResult.Error("Loading failed")))
+    }
+
+    @Test
+    fun `When action isn't a known type, reduceLoading should return stored loading value`() {
+        assertTrue(reducerLoading(true, UnknownEvent()))
+    }
+
+    //reduceCodeCorrect tests
+    @Test
+    fun `When CodeVerificationResult is Loading, reduceCodeCorrect should return false`() {
+        assertFalse(reduceCodeCorrect(true, CodeVerificationResult.Loading))
+    }
+
+    @Test
+    fun `When CodeVerificationResult is Success, reduceCodeCorrect should return the value from the Server`() {
+        assertTrue(reduceCodeCorrect(false, CodeVerificationResult.Success(true)))
+    }
+
+    @Test
+    fun `When CodeVerificationResult is Error, reduceCodeCorrect should return false`() {
+        assertFalse(reduceCodeCorrect(true, CodeVerificationResult.Error("Loading failed")))
+    }
+
+    @Test
+    fun `When action isn't a known type, reduceCodeCorrect should return stored loading value`() {
+        assertTrue(reduceCodeCorrect(true, UnknownEvent()))
+    }
+}
