bde37ca778be3ce51f80512209cf19a3db9c5504
Jonathon Smyth <jsmyth886@gmail.com>
2020-04-03 21:54:24 +0100

    Update Epic to send off ApiRequest
    Send ApiRequest with Code once CodeEntered Event has been fired

--- app/src/main/java/com/puddlealley/flux/store/device/SecretCaveEpic.kt
+++ app/src/main/java/com/puddlealley/flux/store/device/SecretCaveEpic.kt
@@ -5,11 +5,12 @@ import com.puddlealley.flux.store.AppState
 import com.puddlealley.splash.core.createEpic
 import io.reactivex.Observable
 import io.reactivex.rxkotlin.ofType
+import java.util.*
 
 fun secretCarvEpic(apiRequests: ApiRequests) =
-    createEpic<AppState> { (actions) ->
-        // this is a clue.
-        val codeEntered = actions.ofType<SecretCaveEvents.CodeEntered>()
-
-        Observable.empty()
+    createEpic<AppState> { (events, state) ->
+        events.ofType<SecretCaveEvents.CodeEntered>().flatMap {
+            val secretCaveState = state.blockingFirst().secretCaveState
+            apiRequests.codeVerification(secretCaveState.enteredCode)
+        }
     }
